aae49bde91b3da8c7efc56291834034c18673a3f
diamond <aaron.diamond2@yahoo.co.nz>
2023-10-11 23:33:08 +1300

    added the partial complete nav menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,32 +1,53 @@
 import React from "react";
-import { Calendar, BellRinging, Briefcase } from "@phosphor-icons/react";
+import {
+  Calendar,
+  BellRinging,
+  Briefcase,
+  SignOut,
+  ArrowFatLinesRight,
+  ArrowFatLinesLeft,
+} from "@phosphor-icons/react";
 
 import "../../styles/navstyles.css";
 const Nav = () => {
   return (
     <>
       <div className="navside__wrapper">
+        <div>
+          <div className="nav-date__wrapper">
+            <p>1/12/2023</p>
+            <div>
+              <button className="nav-menu-arrow__button">
+                <ArrowFatLinesLeft size={16} color="#454545" />
+              </button>
+            </div>
+          </div>
+        </div>
         <ul>
           <li>
             <button>
-              <Calendar size={16} color="#454545" />
+              <Calendar size={20} color="#454545" />
             </button>
 
-            <label>Calendar</label>
+            <label className="calendar-label">Calendar</label>
           </li>
           <li>
             <button>
-              <BellRinging size={16} color="#454545" />
+              <BellRinging size={20} color="#454545" />
             </button>
+            <label className="notification-label">Notification</label>
           </li>
           <li>
             <button>
-              <Briefcase size={16} color="#454545" />
+              <Briefcase size={20} color="#454545" />
             </button>
+            <label className="domain-label">Domain</label>
           </li>
           <br />
           <li className="logout-btn">
-            <button>Logout</button>
+            <button>
+              <SignOut size={20} color="#454545" />
+            </button>
           </li>
         </ul>
       </div>
