2680c73d1cbe1a2a75c858c73d861335b16d0d02
diamond <aaron.diamond2@yahoo.co.nz>
2023-10-24 23:59:50 +1300

    added dropdown list

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
 
+import "../../styles/calendarstyles.css";
 const Calendar = () => {
+  const today = new Date();
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const [selectedMonth, setSelectedMonth] = useState(months[today.getMonth()]);
+  const [openDropDownList, setOpenDropDownList] = useState(false);
   return (
     <div>
-      <h1>Calendar!</h1>
+      <div className="month-selection__wrapper">
+        <button
+          className="month-selection__dropdown"
+          onClick={() => setOpenDropDownList(!openDropDownList)}
+        >
+          {selectedMonth} &nbsp; &nbsp; &nbsp;{" "}
+          {openDropDownList === false ? (
+            <CaretDown size={11} color="#202020" />
+          ) : (
+            <CaretUp size={11} color="#202020" />
+          )}
+        </button>
+
+        {openDropDownList === true ? (
+          <div className="month-selection-option__wrapper">
+            {months.map((months) => (
+              <button
+                className="month-selection__option"
+                onClick={(e) => setSelectedMonth(e.target.value)}
+                value={months}
+              >
+                {months}
+              </button>
+            ))}
+          </div>
+        ) : (
+          ""
+        )}
+        {/* <select
+          className="month-selection__dropdown"
+          onChange={(e) => setSelectedMonth(e.target.value)}
+          value={selectedMonth}
+        >
+          {months.map((months) => (
+            <option value={months} className="opt">
+              {months}
+            </option>
+          ))}
+        </select> */}
+      </div>
     </div>
   );
 };
