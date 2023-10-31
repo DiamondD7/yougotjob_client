e47d0f4aacc9293e98bec973e29b9406009e4424
diamond <aaron.diamond2@yahoo.co.nz>
2023-10-31 23:59:33 +1300

    added the logic that if the year changes. The leap year will take effect

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { CaretDown, CaretUp } from "@phosphor-icons/react";
-import { monthList, currentYear } from "../../assets/js/months";
+import {
+  monthList,
+  currentYear,
+  handleYearChange,
+} from "../../assets/js/months";
 
 import "../../styles/calendarstyles.css";
 const Calendar = () => {
@@ -10,40 +14,51 @@ const Calendar = () => {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ]; //reference to the days of each month
-  const [selectedMonth, setSelectedMonth] = useState("");
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const [selectedMonth, setSelectedMonth] = useState(months[today.getMonth()]);
   const [selectedMaxMonthDays, setSelectedMaxMonthDays] = useState(0);
   const [openDropDownList, setOpenDropDownList] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [fullDate, setFullDate] = useState(
+    handleYearChange(today.getFullYear())
+  );
 
   const setMonth = (e, months) => {
     //handle the onclick function when choosing a month
-    e.preventDefault();
     setSelectedMonth(months.monthName);
     setSelectedMaxMonthDays(months.maxDays);
     setOpenDropDownList(!openDropDownList);
   };
-
   useEffect(() => {
-    const months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    monthList.map((month) => {
-      if (month.monthName === months[today.getMonth()]) {
+    refreshCalendarList();
+  }, [fullDate]);
+
+  const refreshCalendarList = () => {
+    fullDate.map((month) => {
+      if (month.monthName === selectedMonth) {
         setSelectedMaxMonthDays(month.maxDays);
         setSelectedMonth(month.monthName);
       } //handles the current month and the current month's max days.
     });
-  }, []);
+  };
+
+  useEffect(() => {
+    setFullDate(handleYearChange(parseInt(selectedYear)));
+  }, [selectedYear]);
   return (
     <div>
       <div className="calendar-headers__wrapper">
@@ -61,6 +76,14 @@ const Calendar = () => {
           </button>
         </div>
 
+        <div>
+          <input
+            type="text"
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(e.target.value)}
+          />
+        </div>
+
         <div className="filter-calendar__wrapper">
           <button>Agenda</button>
           <button>Week</button>
@@ -70,7 +93,7 @@ const Calendar = () => {
 
       {openDropDownList === true ? (
         <div className="month-selection-option__wrapper">
-          {monthList.map((months) => (
+          {fullDate.map((months) => (
             <button
               className={`month-selection__option ${
                 selectedMonth === months.monthName ? "currentMonth" : ""
