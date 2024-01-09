b0a06673b005d3fcf055aa5c8e58261079d8ed3a
diamond <aaron.diamond2@yahoo.co.nz>
2024-01-10 07:34:27 +1300

    added contents in the communication

--- src/components/Communication/Communication.jsx
+++ src/components/Communication/Communication.jsx
@@ -1,9 +1,164 @@
 import React from "react";
 
+import "../../styles/communicationstyles.css";
 const Communication = () => {
+  //add the background highlight when clicked once i have data.
   return (
     <div>
-      <h1>Communication</h1>
+      <div className="communication-profiles__wrapper">
+        <input
+          className="search-profile__input"
+          type="text"
+          placeholder="search"
+        />
+
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Aaron Sierra</label>
+        </button>
+
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Raeann Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Mikel Sierra</label>
+        </button>
+
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+        <button className="profile-label__wrapper">
+          <img
+            src="https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="test-picture"
+            className="test-image"
+          />
+          <label>Sean Sierra</label>
+        </button>
+      </div>
     </div>
   );
 };
