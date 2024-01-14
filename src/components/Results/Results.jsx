d262378f500c65061a819116d406431515628aaf
diamond <aaron.diamond2@yahoo.co.nz>
2024-01-14 20:32:04 +1300

    added mock data in the result component

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -6,15 +6,45 @@ const Results = () => {
     <div>
       <div className="patients-results-container__wrapper">
         <div className="patients-results-container">
+          <div>
+            <p className="patient-result-date">5/12/2023</p>
+          </div>
           <div className="patient-sub-details__wrapper">
             <h3 className="patient-name__text">Aaron Sierra</h3>
             <p className="patient-id__text">ID:30004997</p>
           </div>
+          <div className="patient-diagnosis__wrapper">
+            <p>
+              jdokiajdkoia Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Repellendus dolores minima cupiditate officiis voluptatum,
+              sed laborum amet similique atque assumenda minus eligendi libero
+              mollitia illum, fugit corporis aspernatur facilis nemo! Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+              doloribus exercitationem, ad delectus, nostrum sit magni veniam
+              illo corporis minus iusto dolor, nulla rerum necessitatibus
+              reprehenderit. Quam fuga nostrum dolorum!
+            </p>
+          </div>
+          <div>
+            <button className="patient-view__btn">view</button>
+          </div>
         </div>
         <div className="patients-results-container">
+          <div>
+            <p className="patient-result-date">5/12/2023</p>
+          </div>
           <div className="patient-sub-details__wrapper">
             <h3 className="patient-name__text">Tiara Tubalado </h3>
             <p className="patient-id__text">ID:35206327</p>
+          </div>
+          <div className="patient-diagnosis__wrapper">
+            <p>
+              jdokiajdkoia Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Repellendus dolores minima cupiditate offici
+            </p>
+          </div>
+          <div>
+            <button className="patient-view__btn">view</button>
           </div>
         </div>
       </div>
