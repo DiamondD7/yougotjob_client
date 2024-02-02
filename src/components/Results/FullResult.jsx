7eedd51d7d618a8ee2fcda117a1affa97923f31d
diamond <aaron.diamond2@yahoo.co.nz>
2024-02-03 00:24:25 +1300

    added necessary data for the table

--- src/components/Results/FullResult.jsx
+++ src/components/Results/FullResult.jsx
@@ -9,7 +9,14 @@ const FullResult = ({ setOpenResult }) => {
       >
         back
       </button>
-      <h1>Hello</h1>
+      <div className="fullres-profile__wrapper">
+        <h3>Johnson Martin Leaso Henderson</h3>
+        <p>DOB: 2 November 1998</p>
+        <p>Age: 35</p>
+        <p>Nationality: Mexican</p>
+        <p>Height: 166cm</p>
+        <p>Weight: 75kg</p>
+      </div>
     </div>
   );
 };
