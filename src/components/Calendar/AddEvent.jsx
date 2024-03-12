9ecb4ad07624f9fabe3066919987c36e2d186541
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-03-12 21:09:50 +1300

    add event form

--- src/components/Calendar/AddEvent.jsx
+++ src/components/Calendar/AddEvent.jsx
@@ -14,7 +14,130 @@ const AddEvent = ({ setOpenAddEventModal }) => {
         <h1 style={{ textAlign: "center" }}>Create Appointment</h1>
         <div className="addevent-subcontainer__wrapper">
           <div className="form-details__wrapper">
-            <h3>Full Details</h3>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  First Name
+                </p>
+              </div>
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  Last Name
+                </p>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                marginTop: "10px",
+              }}
+            >
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  Contact number
+                </p>
+              </div>
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  Email Address
+                </p>
+              </div>
+            </div>
+            <div style={{ marginTop: "30px" }}>
+              <input className="form-address__input" type="text" />
+              <p
+                style={{
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  margin: "5px 0 0 16px",
+                }}
+              >
+                Street Address
+              </p>
+              <input className="form-address__input" type="text" />
+              <p
+                style={{
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  margin: "5px 0 0 16px",
+                }}
+              >
+                Street Address Line 2
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                marginTop: "10px",
+              }}
+            >
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  City
+                </p>
+              </div>
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  City
+                </p>
+              </div>
+              <div>
+                <input className="form__input" type="text" />
+                <p
+                  style={{
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    marginTop: "5px",
+                  }}
+                >
+                  State/Province
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
