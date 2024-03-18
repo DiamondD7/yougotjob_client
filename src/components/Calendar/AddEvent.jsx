dd3712ea0237fc4d6759ae8f1f53180be6d7312b
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-03-18 13:28:24 +1300

    styled the appointment creation

--- src/components/Calendar/AddEvent.jsx
+++ src/components/Calendar/AddEvent.jsx
@@ -9,31 +9,35 @@ const AddEvent = ({ setOpenAddEventModal }) => {
           className="addevent-close__btn"
           onClick={() => setOpenAddEventModal(false)}
         >
-          <X size={30} color="#202020" />
+          <X size={30} color="#f3f3f3" />
         </button>
-        <h1 style={{ textAlign: "center" }}>Create Appointment</h1>
+        <h1 style={{ color: "#f3f3f3", marginLeft: "100px" }}>
+          Create Appointment
+        </h1>
         <div className="addevent-subcontainer__wrapper">
           <div className="form-details__wrapper">
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <div style={{ display: "flex" }}>
               <div>
-                <input className="form__input" type="text" />
+                <input className="form-details__input" type="text" />
                 <p
                   style={{
                     fontSize: "12px",
                     fontWeight: "bold",
                     marginTop: "5px",
+                    marginLeft: "16px",
                   }}
                 >
                   First Name
                 </p>
               </div>
               <div>
-                <input className="form__input" type="text" />
+                <input className="form-details__input" type="text" />
                 <p
                   style={{
                     fontSize: "12px",
                     fontWeight: "bold",
                     marginTop: "5px",
+                    marginLeft: "16px",
                   }}
                 >
                   Last Name
@@ -43,29 +47,30 @@ const AddEvent = ({ setOpenAddEventModal }) => {
             <div
               style={{
                 display: "flex",
-                justifyContent: "space-around",
                 marginTop: "10px",
               }}
             >
               <div>
-                <input className="form__input" type="text" />
+                <input className="form-details__input" type="text" />
                 <p
                   style={{
                     fontSize: "12px",
                     fontWeight: "bold",
                     marginTop: "5px",
+                    marginLeft: "16px",
                   }}
                 >
                   Contact number
                 </p>
               </div>
               <div>
-                <input className="form__input" type="text" />
+                <input className="form-details__input" type="text" />
                 <p
                   style={{
                     fontSize: "12px",
                     fontWeight: "bold",
                     marginTop: "5px",
+                    marginLeft: "16px",
                   }}
                 >
                   Email Address
@@ -122,7 +127,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                     marginTop: "5px",
                   }}
                 >
-                  City
+                  Postal / Zip Code
                 </p>
               </div>
               <div>
@@ -138,6 +143,20 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 </p>
               </div>
             </div>
+            <div>
+              <textarea className="form-comments__textarea"></textarea>
+              <p
+                style={{
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  marginTop: "2px",
+                  marginLeft: "16px",
+                }}
+              >
+                Comments
+              </p>
+            </div>
+            <button className="form-submit__btn">Submit</button>
           </div>
         </div>
       </div>
