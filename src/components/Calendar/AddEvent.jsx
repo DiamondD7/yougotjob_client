e8b0f981a3e69c3459acf9566cee5c535a39191e
diamond <aaron.diamond2@yahoo.co.nz>
2024-03-20 18:58:13 +1300

    added more context in the appointment

--- src/components/Calendar/AddEvent.jsx
+++ src/components/Calendar/AddEvent.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { X } from "@phosphor-icons/react";
 
 const AddEvent = ({ setOpenAddEventModal }) => {
+  const [formSearchField, setFormSearchField] = useState("");
+
+  const testProfile = [
+    {
+      id: 55,
+      picture:
+        "https://images.unsplash.com/photo-1682687982502-1529b3b33f85?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      nhi: "NHNX8438",
+      firstName: "Henry",
+      lastName: "Tood",
+      nationality: "Indian",
+      dob: "28 Jan 1998",
+      age: "25",
+      height: "160",
+      weight: "60",
+      email: "henry@gmail.com",
+    },
+    {
+      id: 56,
+      picture:
+        "https://images.unsplash.com/photo-1705798543468-5b951da25e1e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      nhi: "KJHN8438",
+      firstName: "Helen",
+      lastName: "Tood",
+      nationality: "Indian",
+      dob: "2 Nov 1991",
+      age: "32",
+      height: "150",
+      weight: "60",
+      email: "helen@gmail.com",
+    },
+    {
+      id: 57,
+      picture:
+        "https://images.unsplash.com/photo-1699901853492-8bc942fc6a5c?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      nhi: "CDEK8438",
+      firstName: "Jack",
+      lastName: "Armstrong",
+      nationality: "Kiwi",
+      dob: "15 Aug 1997",
+      age: "26",
+      height: "180",
+      weight: "77",
+      email: "jackarm@gmail.com",
+    },
+  ];
+  const filteredFormData = testProfile.filter(
+    (data) =>
+      data.firstName.toLowerCase().includes(formSearchField.toLowerCase()) ||
+      data.lastName.toLowerCase().includes(formSearchField.toLowerCase()) ||
+      data.nhi.toLowerCase().includes(formSearchField.toLowerCase())
+  );
+
   return (
     <div>
       <div className="addevent__wrapper">
@@ -12,16 +65,69 @@ const AddEvent = ({ setOpenAddEventModal }) => {
           <X size={30} color="#f3f3f3" />
         </button>
         <h1 style={{ color: "#f3f3f3", marginLeft: "100px" }}>
-          Create Appointment
+          Appointment Form
         </h1>
         <div className="addevent-subcontainer__wrapper">
+          <div className="form-search-users__wrapper">
+            <h4 style={{ textAlign: "center" }}>Search for existing user</h4>
+            <input
+              type="text"
+              className="form-search-users__input"
+              placeholder="Patient ID / Name / NHI"
+              onChange={(e) => setFormSearchField(e.target.value)}
+            />
+            {filteredFormData.length === 0 || formSearchField === "" ? (
+              <div></div>
+            ) : (
+              <div className="form-search-result__wrapper">
+                {filteredFormData.map((data, index) => (
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "10px",
+                      marginTop: "3px",
+                      width: "100%",
+                      backgroundColor: "transparent",
+                      border: "none",
+                      cursor: "pointer",
+                    }}
+                    key={index}
+                  >
+                    <img
+                      className="form-search-profile-picture__img"
+                      src={data.picture}
+                      alt="profilepic"
+                    />
+                    <p style={{ fontSize: "11px" }}>
+                      {data.firstName} {data.lastName}
+                    </p>
+                    <p style={{ fontSize: "11px" }}>{data.nhi}</p>
+                    <p style={{ fontSize: "11px" }}>{data.height}cm</p>
+                    <p style={{ fontSize: "11px" }}>{data.weight}kg</p>
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
           <div className="form-details__wrapper">
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: "12px",
+              }}
+            >
+              Or
+            </p>
+            <h4 style={{ textAlign: "center", marginBottom: "10px" }}>
+              Create a new appointment
+            </h4>
             <div style={{ display: "flex" }}>
               <div>
                 <input className="form-details__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                     marginLeft: "16px",
@@ -34,7 +140,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form-details__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                     marginLeft: "16px",
@@ -54,7 +160,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form-details__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                     marginLeft: "16px",
@@ -67,7 +173,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form-details__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                     marginLeft: "16px",
@@ -77,11 +183,11 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 </p>
               </div>
             </div>
-            <div style={{ marginTop: "30px" }}>
+            <div style={{ marginTop: "10px" }}>
               <input className="form-address__input" type="text" />
               <p
                 style={{
-                  fontSize: "12px",
+                  fontSize: "10px",
                   fontWeight: "bold",
                   margin: "5px 0 0 16px",
                 }}
@@ -91,7 +197,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
               <input className="form-address__input" type="text" />
               <p
                 style={{
-                  fontSize: "12px",
+                  fontSize: "10px",
                   fontWeight: "bold",
                   margin: "5px 0 0 16px",
                 }}
@@ -110,7 +216,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                   }}
@@ -122,7 +228,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                   }}
@@ -134,7 +240,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
                 <input className="form__input" type="text" />
                 <p
                   style={{
-                    fontSize: "12px",
+                    fontSize: "10px",
                     fontWeight: "bold",
                     marginTop: "5px",
                   }}
@@ -147,7 +253,7 @@ const AddEvent = ({ setOpenAddEventModal }) => {
               <textarea className="form-comments__textarea"></textarea>
               <p
                 style={{
-                  fontSize: "12px",
+                  fontSize: "10px",
                   fontWeight: "bold",
                   marginTop: "2px",
                   marginLeft: "16px",
