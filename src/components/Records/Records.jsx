a7c2bd9ba5ad246be9ec93782d9d1e633c9674d7
diamond <aaron.diamond2@yahoo.co.nz>
2024-05-20 00:38:47 +1200

    added more context in the record page

--- src/components/Records/Records.jsx
+++ src/components/Records/Records.jsx
@@ -13,6 +13,13 @@ const Records = () => {
           />
           <button className="records-invoice-search__btn">search</button>
         </div>
+        <div className="records-invoice-container__wrapper">
+          {/* <p className="norecordsfound__text">No records found</p> */}
+          <p className="status-outcome__text">Paid</p>
+          <label className="status-outcome__date"> on 15/03/2024</label>
+          <h4>Invoice #77134</h4>
+          <p style={{ fontSize: "11px" }}>Johnson Martin Leoso</p>
+        </div>
       </div>
     </div>
   );
