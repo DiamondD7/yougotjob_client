469c9b798ee78bf9047d7baf4e779ec0b6ac2f31
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-07-06 02:05:10 +1200

    responsiveness

--- src/components/Records/Records.jsx
+++ src/components/Records/Records.jsx
@@ -267,12 +267,12 @@ const Records = () => {
           <label className="status-outcome__date"> on 15/03/2024</label>
           <h4>Invoice #77134</h4>
           <p style={{ fontSize: "11px" }}>Johnson Martin Leoso</p>
-          <div style={{ marginTop: "10px", display: "flex" }}>
+          <div className="records-invoice-display-flex__wrapper">
             <div>
               <PatientDetailsContainer />
               <PractionerDetailsContainer />
             </div>
-            <div>
+            <div className="records-invoice-display-payment-details__wrapper">
               <InvoicePaymentDetails />
               <PaymentDetailsContainer />
               <InvoiceNotesContainer />
