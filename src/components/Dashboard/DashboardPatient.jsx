e80762df7a740d908c9b092353d6cf6f591cf74e
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-07-23 00:18:19 +1200

    added other summary cards

--- src/components/Dashboard/DashboardPatient.jsx
+++ src/components/Dashboard/DashboardPatient.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { User } from "@phosphor-icons/react";
+import {
+  User,
+  Calendar,
+  ChatCenteredText,
+  EnvelopeOpen,
+} from "@phosphor-icons/react";
 
 import "../../styles/patientdashboard.css";
-const DashboardPatient = () => {
+const SummaryCards = () => {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -47,7 +52,7 @@ const DashboardPatient = () => {
 
   return (
     <div>
-      <div style={{ padding: "10px" }}>
+      <div style={{ padding: "10px", display: "flex", gap: "10px" }}>
         <div className="header-cards__wrapper">
           <div className="dashboard-card-title__wrapper">
             <div>
@@ -63,7 +68,7 @@ const DashboardPatient = () => {
           </div>
           <div className="dashboard-patient-card-details__wrapper">
             <h2 className="dashboard-patient-card-details-h2-schedule">
-              23/01/2024 12:40pm
+              27/08/2024 12:40pm
             </h2>
             <h2 className="dashboard-patient-card-details-h2-countdown">
               {days}
@@ -74,6 +79,55 @@ const DashboardPatient = () => {
             </h2>
           </div>
         </div>
+
+        <div className="dashboard-cards__wrapper">
+          <div className="dashboard-card-title__wrapper">
+            <div>
+              <Calendar size={19} color="#9DCD5A" />
+            </div>
+            <h5>Previous appointment</h5>
+          </div>
+          <div className="dashboard-patient-card__wrapper">
+            <div>
+              <h3>Dr. Johnson</h3>
+              <label className="patient-card-id__label">ID:30004997</label>
+            </div>
+          </div>
+
+          <div className="dashboard-patient-card-details__wrapper">
+            <h2 className="dashboard-patient-card-details-h2-schedule">
+              19/01/2024 8:40am
+            </h2>
+            <div className="previous-patient-view__wrapper">
+              <button className="previous-patient-view__btn">view</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="dashboard-cards__wrapper">
+          <div className="dashboard-card-title__wrapper">
+            <div>
+              <ChatCenteredText size={19} color="#9DCD5A" />
+            </div>
+            <h5>Messages</h5>
+          </div>
+          <div style={{ textAlign: "center", marginTop: "30px" }}>
+            <EnvelopeOpen size={25} color="rgba(0,0,0,0.4)" />
+            <p style={{ color: "rgba(0,0,0,0.3)", fontSize: "13px" }}>
+              No new messages
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DashboardPatient = () => {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <SummaryCards />
       </div>
     </div>
   );
