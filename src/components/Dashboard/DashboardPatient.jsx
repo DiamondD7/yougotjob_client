a2eb5e5ab2e62bf84c760315091c1b35706cf0d0
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-07-23 00:04:13 +1200

    added previous appointment

--- src/components/Dashboard/DashboardPatient.jsx
+++ src/components/Dashboard/DashboardPatient.jsx
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { User } from "@phosphor-icons/react";
 
 import "../../styles/patientdashboard.css";
 const DashboardPatient = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const [inputDate, setInputDate] = useState("7 Aug 2024 20:00"); //can be changed to mm/dd/yyyy
+
+  //handles countdown
+  useEffect(() => {
+    const changingDate = new Date(inputDate);
+    const currentDate = new Date();
+    const totalSeconds = (changingDate - currentDate) / 1000;
+
+    if (totalSeconds <= 0) {
+      setDays(0);
+      setHours(0);
+      setMinutes(0);
+      setSeconds(0);
+    } else if (totalSeconds > 0) {
+      setDays(Math.floor(totalSeconds / 3600 / 24));
+      setHours(Math.floor(totalSeconds / 3600) % 24);
+      setMinutes(Math.floor(totalSeconds / 60) % 60);
+    }
+
+    let interval = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 0) {
+          clearInterval(interval);
+        }
+        return Math.floor(totalSeconds % 60);
+      });
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts or the effect is re-run
+    return () => clearInterval(interval);
+  }, [days, minutes, hours, seconds]);
+
+  const day = <label className="time-indication">d</label>;
+  const hour = <label className="time-indication">hr</label>;
+  const min = <label className="time-indication">min</label>;
+  const sec = <label className="time-indication">sec</label>;
+
   return (
     <div>
       <div style={{ padding: "10px" }}>
-        <div className="header-cards__wrapper"></div>
+        <div className="header-cards__wrapper">
+          <div className="dashboard-card-title__wrapper">
+            <div>
+              <User size={19} color="#9DCD5A" />
+            </div>
+            <h5>Next appointment</h5>
+          </div>
+          <div className="dashboard-patient-card__wrapper">
+            <div>
+              <h3>Dr. Johnson</h3>
+              <label className="patient-card-id__label">ID:30004997</label>
+            </div>
+          </div>
+          <div className="dashboard-patient-card-details__wrapper">
+            <h2 className="dashboard-patient-card-details-h2-schedule">
+              23/01/2024 12:40pm
+            </h2>
+            <h2 className="dashboard-patient-card-details-h2-countdown">
+              {days}
+              {day} : {hours}
+              {hour} : {minutes}
+              {min} : {seconds}
+              {sec}
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
