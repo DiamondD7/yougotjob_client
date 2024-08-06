32786ecf526e6face69e06a087680150890b3c9a
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-08-06 23:01:26 +1200

    added the chosenConvo

--- src/components/Communication/PatientComms.jsx
+++ src/components/Communication/PatientComms.jsx
@@ -8,6 +8,7 @@ const PatientComms = () => {
   const [searchData, setSearchData] = useState([]);
   const [searchLoad, setSearchLoad] = useState(true);
   const [searchField, setSearchField] = useState("");
+  const [chosenConvo, setChosenConvo] = useState([]);
 
   useEffect(() => {
     if (searchField.length > 0) {
@@ -28,6 +29,11 @@ const PatientComms = () => {
   const filteredData = searchData.filter((data) =>
     data.fullName.toLowerCase().includes(searchField.toLowerCase())
   );
+
+  const handleOpenConvo = (data) => {
+    setSearchField("");
+    setChosenConvo(data);
+  };
   return (
     <div>
       <div className="communication-container__wrapper">
@@ -36,6 +42,7 @@ const PatientComms = () => {
             className="search-profile__input"
             type="text"
             placeholder="search"
+            value={searchField}
             onChange={(e) => setSearchField(e.target.value)}
           />
 
@@ -49,7 +56,10 @@ const PatientComms = () => {
               {searchLoad === false ? (
                 filteredData.map((data, index) => (
                   <div className="search-details__wrapper" key={index}>
-                    <button className="search-details__btns">
+                    <button
+                      className="search-details__btns"
+                      onClick={() => handleOpenConvo(data)}
+                    >
                       <p>Dr.{data.fullName}</p>{" "}
                       <p>#{data.registrationNumber}</p>
                     </button>
@@ -80,66 +90,70 @@ const PatientComms = () => {
           <button className="profile-label__wrapper">Sean Sierra</button> */}
         </div>
 
-        <div>
-          <div className="convo-details__wrapper">
-            <h3>Dr Raeann Sierra</h3>
+        {chosenConvo.length <= 0 ? (
+          <div></div>
+        ) : (
+          <div>
+            <div className="convo-details__wrapper">
+              <h3>Dr {chosenConvo.fullName}</h3>
+            </div>
+            <div className="convo-container__wrapper">
+              {/* <div className="recieved-message-container__wrapper">
+                <div className="recieved-message__wrapper">
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                    ipsam distinctio assumenda maxime nobis, laborum alias
+                    facilis blanditiis doloribus excepturi ea, similique, libero
+                    dicta minima inventore sit ipsum perferendis tempora. Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                    repudiandae nostrum reiciendis nemo non quia doloribus,
+                    cupiditate deserunt. Minus itaque quis eveniet vitae
+                    excepturi porro, error enim cumque mollitia reiciendis.
+                  </p>
+                </div>
+                <label className="message-timestamp">12:30 PM</label>
+              </div>
+              <div className="user-message-container__wrapper">
+                <label className="message-timestamp">12:33 PM</label>
+                <div className="user-message__wrapper">
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                    ipsam distinctio assumenda maxime nobis, laborum alias
+                    facilis blanditiis doloribus excepturi ea, similique, libero
+                    dicta minima inventore sit ipsum perferendis tempora. Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                    repudiandae nostrum reiciendis nemo non quia doloribus,
+                    cupiditate deserunt. Minus itaque quis eveniet vitae
+                    excepturi porro, error enim cumque mollitia reiciendis.
+                  </p>
+                </div>
+              </div>
+              <div className="recieved-message-container__wrapper">
+                <div className="recieved-message__wrapper">
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                    ipsam distinctio assumenda maxime nobis, laborum alias
+                    facilis blanditiis doloribus excepturi ea, similique, libero
+                    dicta minima inventore sit ipsum perferendis tempora. Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                    repudiandae nostrum reiciendis nemo non quia doloribus,
+                    cupiditate deserunt. Minus itaque quis eveniet vitae
+                    excepturi porro, error enim cumque mollitia reiciendis.
+                  </p>
+                </div>
+                <div>
+                  <label className="message-timestamp">12:38 PM</label>
+                </div>
+              </div> */}
+            </div>
+            <textarea
+              className="textbox-message"
+              placeholder="Write your message here..."
+            ></textarea>
+            <br />
+            <button className="send-btn">send</button>
           </div>
-          <div className="convo-container__wrapper">
-            <div className="recieved-message-container__wrapper">
-              <div className="recieved-message__wrapper">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
-                  ipsam distinctio assumenda maxime nobis, laborum alias facilis
-                  blanditiis doloribus excepturi ea, similique, libero dicta
-                  minima inventore sit ipsum perferendis tempora. Lorem ipsum
-                  dolor sit amet consectetur adipisicing elit. Eligendi
-                  repudiandae nostrum reiciendis nemo non quia doloribus,
-                  cupiditate deserunt. Minus itaque quis eveniet vitae excepturi
-                  porro, error enim cumque mollitia reiciendis.
-                </p>
-              </div>
-              <label className="message-timestamp">12:30 PM</label>
-            </div>
-            <div className="user-message-container__wrapper">
-              <label className="message-timestamp">12:33 PM</label>
-              <div className="user-message__wrapper">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
-                  ipsam distinctio assumenda maxime nobis, laborum alias facilis
-                  blanditiis doloribus excepturi ea, similique, libero dicta
-                  minima inventore sit ipsum perferendis tempora. Lorem ipsum
-                  dolor sit amet consectetur adipisicing elit. Eligendi
-                  repudiandae nostrum reiciendis nemo non quia doloribus,
-                  cupiditate deserunt. Minus itaque quis eveniet vitae excepturi
-                  porro, error enim cumque mollitia reiciendis.
-                </p>
-              </div>
-            </div>
-            <div className="recieved-message-container__wrapper">
-              <div className="recieved-message__wrapper">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
-                  ipsam distinctio assumenda maxime nobis, laborum alias facilis
-                  blanditiis doloribus excepturi ea, similique, libero dicta
-                  minima inventore sit ipsum perferendis tempora. Lorem ipsum
-                  dolor sit amet consectetur adipisicing elit. Eligendi
-                  repudiandae nostrum reiciendis nemo non quia doloribus,
-                  cupiditate deserunt. Minus itaque quis eveniet vitae excepturi
-                  porro, error enim cumque mollitia reiciendis.
-                </p>
-              </div>
-              <div>
-                <label className="message-timestamp">12:38 PM</label>
-              </div>
-            </div>
-          </div>
-          <textarea
-            className="textbox-message"
-            placeholder="Write your message here..."
-          ></textarea>
-          <br />
-          <button className="send-btn">send</button>
-        </div>
+        )}
       </div>
     </div>
   );
