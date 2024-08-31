7347816c4623323efed4e04e709f07d14a9a04b8
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2024-08-31 20:21:33 +1200

    update Model data

--- src/components/Communication/PatientComms.jsx
+++ src/components/Communication/PatientComms.jsx
@@ -66,7 +66,7 @@ const ChatConvo = ({
       body: JSON.stringify({
         chatHistoryId: chatId,
         recipientId: chosenConvo.id, //this is the id for the chosen convo in the search to get the chatHistoryId to check whether chat already exists
-        userId: currentUserId,
+        SenderId: currentUserId,
       }),
     })
       .then((res) => res.json())
@@ -124,8 +124,8 @@ const ChatConvo = ({
         Accept: "application/json",
       },
       body: JSON.stringify({
-        UserInitiatorId: id,
-        UserRecipientId: chosenConvo.id,
+        UserInitiatorId: id, //Patient is always the initiator
+        UserRecipientId: chosenConvo.id, //Health Practitioners are always the Recipient
         InitiatorName: patient.fullName,
         RecipientName: chosenConvo.fullName,
         Created: localISOTime,
@@ -168,12 +168,23 @@ const ChatConvo = ({
     const id = parseInt(sessionStorage.getItem("id"));
     const dateNow = new Date();
 
-    console.log(chatHistoryId);
     const localISOTime = new Date(
       dateNow.getTime() - dateNow.getTimezoneOffset() * 60000
     )
       .toISOString()
       .slice(0, -1); // Remove the 'Z' in example: 2024-08-08T12:34:56.789Z which indicates UTC.
+
+    //Initialisation and conditioning ----
+    let userInitiator = chosenConvo.userInitiatorId;
+    let userRecipient = chosenConvo.userRecipientId;
+
+    if (userInitiator === undefined && userRecipient === undefined) {
+      //if the convo is empty then just use the id from session and chosenConvo.id for the practitioner id
+      userInitiator = id;
+      userRecipient = chosenConvo.id;
+    }
+
+    //-----
 
     fetch(AddChatMessage, {
       method: "POST",
@@ -183,8 +194,9 @@ const ChatConvo = ({
       },
       body: JSON.stringify({
         ChatHistoryId: chatHistoryId,
-        UserId: id,
-        RecipientId: chosenConvo.id,
+        SenderId: id, //senderId is the sender of the message
+        InitiatorId: userInitiator, //initiatorid is the one that initiates the convo which is Patient
+        RecipientId: userRecipient, //recipientId is the one that reciever of the convo which is the health pracitioners/professionals
         message: messageField,
         CreatedAt: localISOTime,
       }),
@@ -356,7 +368,7 @@ const ChatConvo = ({
           //else if the item is equal the todays date, then return "today"
           <p className="message-datestamp">today</p>
         )}
-        {items.userId === currentUserId ? (
+        {items.senderId === currentUserId ? (
           <div className="user-message-container__wrapper">
             {items.isDeleted === false &&
             items.deletedById !== currentUserId &&
