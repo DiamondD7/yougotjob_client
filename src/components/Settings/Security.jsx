af26c16adbc18bc212d6077b7163b607f9c8e42a
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2025-02-06 13:36:05 +1300

    added logic and styling for changing password

--- src/components/Settings/Security.jsx
+++ src/components/Settings/Security.jsx
@@ -4,12 +4,12 @@ import {
   GetaHealthPractitioner,
   UpdateHealthPractitionerData,
 } from "../../assets/js/serverApi";
+import { CheckCircle } from "@phosphor-icons/react";
 
 import "../../styles/securitystyles.css";
-const TwoFactorAuth = ({ userData }) => {
+const TwoFactorAuth = ({ userData, navigate }) => {
   const [editEmailRec, setEditEmailRec] = useState(false);
   const [emailrec, setEmailRec] = useState("");
-  const navigate = useNavigate();
 
   const handleUpdateEmail = async (retry = true) => {
     const id = parseInt(sessionStorage.getItem("id"));
@@ -125,8 +125,114 @@ const TwoFactorAuth = ({ userData }) => {
   );
 };
 
-const PasswordChange = () => {
+const PasswordChange = ({ navigate }) => {
+  const [passwordChangeSuccess, setPasswordChangeSuccess] = useState(false);
   const [activeChangePassword, setActiveChangePassword] = useState(false);
+
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+  const [validPwMatch, setValidPwdMatch] = useState(true);
+  const [errorPw, setErrorPw] = useState(false);
+
+  const [showErrorMsg, setShowErrorMsg] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [userPassData, setUserPassData] = useState({
+    UserPassword: "",
+    NewPassword: "",
+    ConfirmPassword: "",
+  });
+
+  useEffect(() => {
+    const isPwdValid = PWD_REGEX.test(userPassData.NewPassword);
+    if (
+      userPassData.NewPassword !== userPassData.ConfirmPassword ||
+      userPassData.ConfirmPassword === "" ||
+      userPassData.UserPassword === "" ||
+      !isPwdValid
+    ) {
+      setValidPwdMatch(false);
+    } else {
+      setValidPwdMatch(true);
+    }
+  }, [
+    userPassData.NewPassword,
+    userPassData.ConfirmPassword,
+    userPassData.UserPassword,
+  ]);
+
+  const handleChangePassword = async (retry = true) => {
+    const id = parseInt(sessionStorage.getItem("id"));
+    const isPwdValid = PWD_REGEX.test(userPassData.NewPassword);
+    if (!validPwMatch) {
+      console.log("Password do not match");
+      return;
+    }
+    if (!isPwdValid) {
+      setErrorPw(true);
+      return;
+    }
+
+    try {
+      const response = await fetch(UpdateHealthPractitionerData, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          Id: id,
+          UserPassword: userPassData.UserPassword,
+          NewPassword: userPassData.NewPassword,
+        }),
+      });
+
+      if (response.status === 302) {
+        console.warn("301 detected, redirecting...");
+        navigate("/");
+        return;
+      }
+
+      if (response.status === 401 && retry) {
+        console.warn("401 detected, retrying request...");
+        return handleChangePassword(false);
+      }
+
+      if (response.status === 400) {
+        console.warn("400 detected");
+
+        const data = await response.json();
+        setShowErrorMsg(true);
+        setErrorMessage(data.returnStatus.message);
+      }
+
+      if (!response.ok) {
+        throw new Error(`HTTP ERROR: status ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setPasswordChangeSuccess(true); //this is for the success message
+      handleOpenCloseChangePw(); //close the update form
+    } catch (err) {
+      console.log(`Error caught: ${err.message}`);
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setUserPassData({
+      ...userPassData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOpenCloseChangePw = (e) => {
+    e.preventDefault();
+    setActiveChangePassword(false);
+    setShowErrorMsg(false);
+    setErrorMessage("");
+  };
+
   return (
     <div>
       <h3 style={{ color: "#9dcd5a", fontWeight: "bold" }}>Password</h3>
@@ -142,33 +248,84 @@ const PasswordChange = () => {
               name.
             </p>
             {activeChangePassword === false ? (
-              <button
-                className="security-password-change__btn"
-                onClick={() => setActiveChangePassword(true)}
-              >
-                Update
-              </button>
+              <div>
+                <button
+                  className="security-password-change__btn"
+                  onClick={() => setActiveChangePassword(true)}
+                >
+                  Update
+                </button>
+                {passwordChangeSuccess === true ? (
+                  <div
+                    style={{ display: "flex", gap: "10px", marginTop: "50px" }}
+                  >
+                    <CheckCircle size={15} color="limegreen" />
+                    <p
+                      style={{
+                        fontSize: "12px",
+                        color: "limegreen",
+                      }}
+                    >
+                      Password changed successful
+                    </p>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             ) : (
               <div>
+                {showErrorMsg === true ? (
+                  <p className="security-password-error__text">
+                    {errorMessage}
+                  </p>
+                ) : (
+                  ""
+                )}
                 <br />
                 <p className="security-password-input__text">
                   Current Password
                 </p>
-                <input type="password" className="security-password-input" />
+                <input
+                  type="password"
+                  className="security-password-input"
+                  name="UserPassword"
+                  onChange={(e) => handleOnChange(e)}
+                />
                 <p className="security-password-input__text">New Password</p>
-                <input type="password" className="security-password-input" />
+                <input
+                  type="password"
+                  className={`security-password-input ${
+                    validPwMatch === true ? "" : "inputError"
+                  }`}
+                  name="NewPassword"
+                  onChange={(e) => handleOnChange(e)}
+                />
                 <p className="security-password-input__text">
                   Confirm Password
                 </p>
-                <input type="password" className="security-password-input" />
+                <input
+                  type="password"
+                  className={`security-password-input ${
+                    validPwMatch === true ? "" : "inputError"
+                  }`}
+                  name="ConfirmPassword"
+                  onChange={(e) => handleOnChange(e)}
+                />
                 <br />
                 <button
                   className="security-password-cancelchange__btn"
-                  onClick={() => setActiveChangePassword(false)}
+                  onClick={(e) => handleOpenCloseChangePw(e)}
                 >
                   Cancel
                 </button>
-                <button className="security-password-change__btn">
+                <button
+                  className={`security-password-change__btn ${
+                    validPwMatch === true ? "" : "disabledBtn"
+                  }`}
+                  onClick={handleChangePassword}
+                  disabled={validPwMatch === true ? false : true}
+                >
                   Submit
                 </button>
               </div>
@@ -192,6 +349,7 @@ const PasswordChange = () => {
     </div>
   );
 };
+
 const Security = () => {
   const id = parseInt(sessionStorage.getItem("id"));
   const navigate = useNavigate();
@@ -237,10 +395,10 @@ const Security = () => {
 
   return (
     <div>
-      <TwoFactorAuth userData={userData} />
+      <TwoFactorAuth userData={userData} navigate={navigate} />
       <br />
       <br />
-      <PasswordChange />
+      <PasswordChange navigate={navigate} />
     </div>
   );
 };
